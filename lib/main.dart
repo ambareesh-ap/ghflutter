c8ad97665aded92c16218c46f041ba39d34803e0
ambareesh-ap <ambareesh@antpod.io>
2022-12-30 11:34:09 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'strings.dart' as strings;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const GHFlutterApp());
+
+class GHFlutterApp extends StatelessWidget {
+  const GHFlutterApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: strings.appTitle,
+      home: const GHFlutter(),
+    );
+  }
+}
+
+class GHFlutter extends StatefulWidget {
+  const GHFlutter({super.key});
+
+  @override
+  State<GHFlutter> createState() => _GHFlutterState();
+}
+
+class _GHFlutterState extends State<GHFlutter> {
+  var _members = <dynamic>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Future<void> _loadData() async {
+    const dataUrl = 'https://api.github.com/orgs/raywenderlich/members';
+    final response = await http.get(Uri.parse(dataUrl));
+    setState(() {
+      _members = json.decode(response.body) as List;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Widget _buildRow(int i) {
+    return ListTile(
+      title: Text('${_members[i]['login']}', style: _biggerFont),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(strings.appTitle),
+      ),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemCount: _members.length,
+          itemBuilder: (BuildContext context, int position) {
+            return _buildRow(position);
+          }),
+    );
+  }
+}
